44aacd0c22347322c111fd14e26606c810238763
SergioHMagalhaes <ssergioh3@gmail.com>
2026-06-22 23:41:50 -0300

    feat: add domain entities

---
+++ src/FCG.Catalog.Domain/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace FCG.Catalog.Domain.Entities;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public ICollection<Game> Games { get; set; } = [];
+}
