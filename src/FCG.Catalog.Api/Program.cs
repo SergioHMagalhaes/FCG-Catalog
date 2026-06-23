ce67f7667600d51a8b34411ec3520200146f1c1c
SergioHMagalhaes <ssergioh3@gmail.com>
2026-06-22 21:42:48 -0300

    chore: add swagger

--- src/FCG.Catalog.Api/Program.cs
+++ src/FCG.Catalog.Api/Program.cs
@@ -1,14 +1,48 @@
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
 
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "FCG - Catalog API",
+        Version = "v1",
+        Description = "API responsável pelo CRUD de jogos e por iniciar o fluxo de compra.",
+        Contact = new OpenApiContact
+        {
+            Name = "FIAP Cloud Games - Catalog API"
+        }
+    });
+
+    options.AddSecurityDefinition("bearer", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Description = "JWT Authorization header using the Bearer scheme."
+    });
+
+    options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+    {
+        [new OpenApiSecuritySchemeReference("bearer", document)] = []
+    });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "FCG Catalog API v1");
+        options.DocumentTitle = "FCG - Catalog API";
+    });
 }
 
 app.UseHttpsRedirection();
