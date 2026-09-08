f1991eaf6fcd94d0e5e6342323818532c467e3c4
SergioHMagalhaes <ssergioh3@gmail.com>
2026-09-08 20:38:05 -0300

    chore: remove ReviewsCollectionName from appsettings

--- src/FCG.Catalog.Infrastructure/Settings/MongoSettings.cs
+++ src/FCG.Catalog.Infrastructure/Settings/MongoSettings.cs
@@ -4,5 +4,4 @@ public class MongoSettings
 {
     public string ConnectionString { get; set; } = default!;
     public string DatabaseName { get; set; } = default!;
-    public string ReviewsCollectionName { get; set; } = default!;
 }
